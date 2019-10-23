0c447caeaf7522a33821d814c9d9454c31a5e23f
lanniao <lijialiang19870402@163.com>
2019-10-23 17:10:05 +0800

    add new simpleFactory class

---
+++ src/com/java/base/factory/SimpleFactory.java
@@ -0,0 +1,14 @@
+package com.java.base.factory;
+
+/**
+ * @ClassName SimpleFactory
+ * @Description TODO
+ * @Author LiJialiang
+ * @Date 2019/10/23 17:07
+ * @Version 1.0
+ **/
+public class SimpleFactory {
+    public static void main(String[] args) {
+        System.out.println("This is simpleFactory class!");
+    }
+}
